e7d5e949228bca2ccd4bb60109034f696e42cee3
elinlarsen <elin_larsen1@hotmail.fr>
2017-07-12 18:42:11 -0400

    add print

--- run_sampling_dataset.py
+++ run_sampling_dataset.py
@@ -10,7 +10,7 @@ from pycocotools.coco import COCO
 import get_objects_categories
 import MH_sampling
 
-path_annotations="/pylon2/ci560op/larsene/data/mscoco/annotations/instances_train2014.json"
+path_annotations="/pylon2/ci560op/larsene/data/mscoco/train2014/instances_train2014.json"
 coco_train=COCO(path_annotations)
 categories= coco_train.loadCats(coco_train.getCatIds())
 d_cat_name=get_objects_categories.build_dict_cat_name_to_cat_id(categories)
@@ -31,8 +31,13 @@ T_fin=1
 tau=2
 nb_iter=10
 
+print("selecting wave file names according to speaker names...")
 wave_file_name_selected=MH_sampling.select_speaker_in_wav(input_path, speakers)
+
+print("selecting image that have 4 captions ...")
 ImgID_selected=MH_sampling.get_nb_caption_per_img(n, wave_file_name_selected)
 ImgID_selected_list=list(ImgID_selected)
 
+
+print("sampling selected images ...")
 final_img_selected=MH_sampling.sample_img_id(d_img_to_cat, ImgID_selected_list, output_path, int(sample_size*train_size), T_0, T_fin, tau, nb_iter,  replace=False)
