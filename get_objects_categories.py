6dedf898eb2b5bf983f33a275633e99c192e887c
elinlarsen <elin_larsen1@hotmail.fr>
2017-07-12 15:51:42 -0400

    link image ID to object categories

---
+++ get_objects_categories.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 17:38:36 2017
+
+@author: elinlarsen
+"""
+import os 
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+import json
+
+# Throughout the API "ann"=annotation, "cat"=category, and "img"=image.
+
+#path_annotations="/pylon2/ci560op/larsene/data/mscoco/annotations/instances_train2014.json"
+#coco_train=COCO(path_annotations)
+#categories= coco_train.loadCats(coco_train.getCatIds())
+
+
+
+def build_dict_cat_name_to_cat_id(categories):
+    dict_id_name={}
+    for dd in categories :
+        dict_id_name[dd['id']]=dd['name']
+    return(dict_id_name)
+    
+def build_dict_cat_name_to_img_id(categories, coco):
+    d={}
+    for cat in categories: 
+        imgIds=coco.getImgIds(catIds=cat['id'])
+        d[cat['name']]=imgIds
+    with open('dict_cat_name_to_img_id.json', 'w') as fp:
+        json.dump(d, fp)
+    return(d)
+
+def build_dict_img_id_to_cat_name(dic_name_to_img):
+    d = {}
+    for key, values in dic_name_to_img.items():
+        for value in values:
+            d.setdefault(value, []).append(key)
+    with open('dict_img_id_to_cat_name.json', 'w') as fp:
+        json.dump(d, fp)
+    return(d)
+
+def dict_nb_value_per_key(dic):    
+    d={}
+    for key, value in dic.items():
+        d[key]=len(value)
+    plt.bar(list(d.keys()), d.values(), color='g')
+    plt.show()
+    return(d)
+
+'''
+#tests
+d_cat_to_img=build_dict_cat_name_to_img_id(categories, coco_train)
+with open('dict_cat_name_to_img_id.json', 'w') as fp:
+    json.dump(d_img_to_cat, fp)
+    
+    
+with open('dict_img_id_to_cat_name.json', 'w') as fp:
+    json.dump(d, fp)
+    
+with open('dict_cat_name_to_img_id.json', 'w') as fp:
+    json.dump(d_img_to_cat, fp)    
+
+with open('dict_cat_id_to_name.json', 'w') as fp:
+    json.dump(dic_id_name, fp)    
+''' 
+    
