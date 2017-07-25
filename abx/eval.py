5e4723c436d4be2c685a8a8cfebc29c015317f21
elinlarsen <elin_larsen1@hotmail.fr>
2017-07-25 17:29:00 -0400

    modify condition of evaluation

--- abx/eval.py
+++ abx/eval.py
@@ -14,9 +14,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""Evaluation program for the Zero Speech Challenge 2017
-
-Track 1: Unsupervised subword modeling
+"""Evaluation program for ABX task done  :
+    - on phoneme across speaker by context ("across")
+    - on phoneme across context by speaker ("within")
+    - on phoneme by context and by speaker ("control")
+    
+    - on word by speaker ("within")
+    - on word across speaker ("across")
+    
+    - on speaker across phoneme by context
 
 """
 
@@ -48,84 +54,102 @@ def lookup(attr, task_items, default=None):
         return default
 
 
-def avg(filename, task_type='across'):
+def avg(filename, on, task_type='across'):
     #task_type = lookup('type', task, 'across')
-    df = pandas.read_csv(filename, sep='\t')
-    if task_type == 'across':
-        # aggregate on context
-        groups = df.groupby(
-            ['speaker_1', 'speaker_2', 'phone_1', 'phone_2'], as_index=False)
-        df = groups['score'].mean()
-    elif task_type == 'within':
-        arr = np.array(map(ast.literal_eval, df['by']))
-        df['speaker']  = [e for e, f, g in arr]
-        df['context'] = [f for e, f, g in arr]
-        #del df['by']
-
-        # aggregate on context
-        groups = df.groupby(['phone_1', 'phone_2'], as_index=False) 
-        #groups = df_within.groupby(['speaker', 'phone_1', 'phone_2'], as_index=False)
-        df = groups['score'].mean()
-    else:
-        raise ValueError('Unknown task type: {0}'.format(task_type))
-
-    # del df['by']
-    # # aggregate on talkers
-    # groups = df.groupby(['context', 'word_1', 'word_2'], as_index=False)
-    # df = groups['score'].mean()
-    # # aggregate on contexts
-    # groups = df.groupby(['word_1', 'word_2'], as_index=False)
-    # df = groups['score'].mean()
+    df = pandas.read_csv(filename, sep='\t', header=0)
     
-    ## Compute confidence interval with bootstrap method : 
+    if on=="phoneme":
+    
+        if task_type == 'across':
+            # aggregate on speakers
+            groups = df.groupby(
+                ['speakerID_1', 'speakerID_2', 'phoneme_1', 'phoneme_2'], as_index=False)
+            df = groups['score'].mean()
+        
+        
+        elif task_type == 'within':
+            arr = np.array(map(ast.literal_eval, df['by']))
+            df['speakerID']  = [e for e, f, g in arr]
+            df['context'] = [f for e, f, g in arr]
 
+            # aggregate on context
+            groups = df.groupby(['phoneme_1', 'phoneme_2'], as_index=False) 
+            df = groups['score'].mean()
+            
+        elif task_type=='control':
+            groups = df.groupby(['phoneme_1', 'phoneme_2'], as_index=False) 
+            df = groups['score'].mean()
+            
+            
+        else:
+            raise ValueError('Unknown task type: {0}'.format(task_type))
+      
+      
+    elif on=="speaker":
+        #aggregate on phoneme
+        groups = df.groupby(['context', 'speakerID_1', 'speakerID_2'], as_index=False)
+        df = groups['score'].mean()
+        
+        
+    elif on=="word":
+    
+        if task_type=='across':
+            #aggregate on speakers
+            groups = df.groupby(['context', 'word_1', 'word_2'], as_index=False)
+            df = groups['score'].mean()
+        elif task_type=='within':
+            # aggregate on contexts
+            groups = df.groupby(['word_1', 'word_2'], as_index=False)
+            df = groups['score'].mean()
+    
+    '''
+    ## Compute confidence interval with bootstrap method :
     boot=df 
     n = 40000
     N = 1000
-
     ci=[]
-    
-    #for i in xrange(0,N):
-    #    bt=boot.merge(pandas.DataFrame(index=np.random.randint(n, size=n)), left_index=True, right_index=True, how='right')
-    #    groups=bt.groupby(['phone_1','phone_2'],as_index=False)
-    #    bt=groups['score'].mean()
-    #    average = bt.mean()[0]
-    #    ci.append(average)
-    #ci=sorted(ci)
-    #confidence=[ci[int(round(0.5*N))],ci[int(round(0.95*N))]]
+    for i in xrange(0,N):
+        bt=boot.merge(pandas.DataFrame(index=np.random.randint(n, size=n)), left_index=True, right_index=True, how='right')
+        groups=bt.groupby(['phone_1','phone_2'],as_index=False)
+        bt=groups['score'].mean()
+        average = bt.mean()[0]
+        ci.append(average)
+    ci=sorted(ci)
+    confidence=[ci[int(round(0.5*N))],ci[int(round(0.95*N))]]
     confidence=[0,0]
-    # aggregate on talker
-    #groups = df.groupby(['phone_1', 'phone_2'], as_index=False)
-    #df = groups['score'].mean()
+     aggregate on talker
+    groups = df.groupby(['phone_1', 'phone_2'], as_index=False)
+    df = groups['score'].mean()
     average = df.mean()[0]
     average = (1.0-average)*100
+    '''    
+    
+    
     return (average,confidence)
 
 
 if __name__ == '__main__':
 
-    import argparse
-
-    # parser (the usage string is specified explicitly because the default
-    # does not show that the mandatory arguments must come before the mandatory
-    # ones; otherwise parsing is not possible beacause optional arguments can
-    # have various numbers of inputs)
     parser = argparse.ArgumentParser(usage="%(prog)s task distance [score]",
                                      description='ABX score computation')
-    # I/O files
+                                     
     g1 = parser.add_argument_group('I/O files')
-    g1.add_argument('filename', help='cvs file with the results for all pairs')
-    g1.add_argument('task', help='across or within speakers')
-    #g1.add_argument('score', help='output file for score')
     
+    g1.add_argument(
+    '-f','--filename', type=str, metavar='<str>',  
+    help='cvs file with the results for all pairs')
+    
+    g1.add_argument(
+    '-t', '--task', type=str, metavar= '<str>', default, 'across', 
+    help='across or within speakers, or control, default is %(default)s')
+    
+    g1.add_argument(
+    '--on', type=str, metavar='<str>', default="phoneme",
+    help='either phoneme,  word or speaker, default is %(default)s')
+
     args = parser.parse_args()
 
-    #if os.path.exists(args.score):
-     #   print("Warning: overwriting score file {}".format(args.score))
-      #  os.remove(args.score)
-    
-
-    print avg(args.filename,args.task)
+    print("the ABX score of the task: " + args.filename.split('.')[0]+ "is :"+str(avg(args.filename, args.on, args.task)))
 
 
 
