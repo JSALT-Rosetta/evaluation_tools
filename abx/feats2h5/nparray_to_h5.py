7b8ee43b86b0c9eaf6ec4a85d2ff23b218b7ab8d
elinlarsen <elin_larsen1@hotmail.fr>
2017-07-31 17:55:11 -0400

    all in one file

--- abx/feats2h5/nparray_to_h5.py
+++ abx/feats2h5/nparray_to_h5.py
@@ -9,6 +9,10 @@ Created on Mon Jul 31 17:13:17 2017
 import os 
 import generate_features
 import argparse
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import h5features
 
 
 #########
@@ -18,6 +22,46 @@ import argparse
 #feats_folder = '/pylon5/ci560op/londel/flickr/ploop_u100_s3_c4/posteriors_ac0.2/test/'
 #output_folder = '/pylon2/ci560op/rachine/data/mscoco/val2014/mfcc'
 
+
+def h5features_from_nparray(input_path, h5f, timefunc=None):
+    """Compute speech features (such as posteriogram) that are in numpy array 
+    in h5features format.
+
+    Parameters:
+    ----------
+    input_path: path of the directory containing the features of audio files in numpy array
+    h5f: str. Name of the h5features file to create.
+    timefunc: callable. Function that returns timestamps for the aforementionned
+        features. By default, it assume a window length of 25 ms and a window
+        step of 10 ms.
+        
+    """
+    files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
+    batch_size = 500
+    features = []
+    times = []
+    internal_files = []
+    i = 0
+    for f in files:
+        if i == batch_size:
+            h5features.write(h5f, '/features/', internal_files, times,
+                             features)
+            features = []
+            times = []
+            internal_files = []
+            i = 0
+        i = i+1
+        features.append(f)
+        if timefunc == None:
+            time = np.arange(f.shape[0], dtype=float) * 0.01 + 0.0025
+        else:
+            time = timefunc(f)
+        times.append(time)
+        internal_files.append(os.path.basename(os.path.splitext(f)[0]))
+    if features:
+        h5features.write(h5f, '/features/',
+                         internal_files, times,
+                         features)
 
 
 #########
