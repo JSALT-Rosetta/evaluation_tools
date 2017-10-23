ec804b054cd1fcf8b9a868bad3b192e55a44aa13
Elin Larsen <elinlarsen@Mac-AC04.local>
2017-10-23 19:09:32 +0200

    floor division

--- abx/feats2h5/lab2npy.py
+++ abx/feats2h5/lab2npy.py
@@ -33,7 +33,7 @@ def lab2npy(input_path, out, frame_rate= 0.01):
    for i in range(len(df)): 
        on=df["onset"].iloc[i]
        off=df["offset"].iloc[i]
-       nb_frame=floor(np.divide((off-on),frame_rate))
+       nb_frame=np.floor_divide((off-on),frame_rate)
        for ff in range(nb_frame): 
            one_hot=np.empty(len(phones))
            for j in range(len(phones)):
@@ -47,7 +47,7 @@ def lab2npy(input_path, out, frame_rate= 0.01):
                
    
 
-def make_npyfiles(input_folder_path,  out, frame_rate,):
+def make_npyfiles(input_folder_path,  out, frame_rate):
    filenames = [f for f in listdir(input_folder_path) if os.path.splitext(f)[-1]==".lab"]
    for f in filenames:
        lab2npy(input_folder_path +"/"+f, out, frame_rate)
