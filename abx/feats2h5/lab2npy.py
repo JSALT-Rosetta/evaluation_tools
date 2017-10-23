dcc722d7524779f956afe1b845faba8b77df9988
Elin Larsen <elinlarsen@Mac-AC04.local>
2017-10-23 19:30:38 +0200

    output name extension

--- abx/feats2h5/lab2npy.py
+++ abx/feats2h5/lab2npy.py
@@ -44,7 +44,7 @@ def lab2npy(input_path, out, frame_rate= 0.01):
                    one_hot[j]=0
            list_feats.append(one_hot)
    arr_feats = np.array(list_feats)
-   np.save(file=out + "/"+ name, arr=arr_feats,  allow_pickle=False)          
+   np.save(file=out + "/"+ name.split(".")[0], arr=arr_feats,  allow_pickle=False)          
                
    
 
