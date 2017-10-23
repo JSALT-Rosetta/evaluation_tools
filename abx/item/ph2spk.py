3475fd2cbcb4456574b7570a31d8e66bc19bdaee
Elin Larsen <elinlarsen@Mac-AC04.local>
2017-10-23 10:55:50 +0200

    fix bug parser 2

--- abx/item/ph2spk.py
+++ abx/item/ph2spk.py
@@ -27,7 +27,7 @@ def phoneme_to_speaker_item_file(item_file):
     
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--i', type=str, help="path to the phoneme file to be transformed to a speaker item file")
+    parser.add_argument('-i', '--item_file', type=str, help="path to the phoneme file to be transformed to a speaker item file")
 
     args=parser.parse_args()
     phoneme_to_speaker_item_file(args.item_file)    
