a23fbaef70f007b8bf4da7aab6d643ca8f494721
Elin Larsen <elinlarsen@Mac-AC04.local>
2017-10-23 19:17:14 +0200

    floor division 3

--- abx/feats2h5/lab2npy.py
+++ abx/feats2h5/lab2npy.py
@@ -12,7 +12,6 @@ import os
 import pandas as pd
 from os import listdir
 import argparse
-from math import floor
 
 def lab2npy(input_path, out, frame_rate= 0.01):
    """
@@ -30,10 +29,12 @@ def lab2npy(input_path, out, frame_rate= 0.01):
    df["offset"]=df["offset"]*10**(-7)   
    phones=['a{}'.format(i) for i in range(1, 49)]   
    list_feats=[]
+   R=np.empty(1)
+   R[0]=frame_rate
    for i in range(len(df)): 
        on=df["onset"].iloc[i]
        off=df["offset"].iloc[i]
-       nb_frame=np.floor_divide((int(off)-int(on)),frame_rate)
+       nb_frame=np.floor_divide((off-on),R[0])
        for ff in range(nb_frame): 
            one_hot=np.empty(len(phones))
            for j in range(len(phones)):
