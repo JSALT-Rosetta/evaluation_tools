13a898de0b17998f6640386e5605b827b62a17aa
Elin Larsen <elinlarsen@Mac-AC04.local>
2017-10-23 11:21:19 +0200

    add default args

--- abx/item/ph2spk.py
+++ abx/item/ph2spk.py
@@ -9,11 +9,11 @@ import os
 import pandas as pd
 import argparse
 
-def phoneme_to_speaker_item_file(item_file): 
+def phoneme_to_speaker_item_file(item_file, unit_colname="#phoneme", spk_colname="speakerID"): 
     
     direc=os.path.dirname(item_file)
     df=pd.read_table(item_file, sep="\t", header=0)
-    df.rename(columns={"speaker_id": "#speakerID", '#phoneme': "phoneme"}, inplace=True)
+    df.rename(columns={spk_colname: "#speakerID", unit_colname: "phoneme"}, inplace=True)
     print(df.columns)
     c=df.columns.tolist()
     i=c.index("#speakerID")
@@ -27,7 +27,9 @@ def phoneme_to_speaker_item_file(item_file):
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--item_file', type=str, help="path to the phoneme file to be transformed to a speaker item file")
+    parser.add_argument('-u', '--unit_colname', type=str, default="#phoneme", help="name of the unit column to take out the #")
+    parser.add_argument('-u', '--spk_colname', type=str, default="speakerID", help="name of the speaker column to add a #")
 
     args=parser.parse_args()
-    phoneme_to_speaker_item_file(args.item_file)    
+    phoneme_to_speaker_item_file(args.item_file, args.unit_colname, args.spk_colname)    
     
